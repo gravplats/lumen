b4f6a9b4d23568fb0353a10e3df77f02dd010e51
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-08-30 23:20:53 +0200

    RavenDB support.

---
+++ src/Lumen.AspNetMvc.Raven/RavenApplicationServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using Lumen.Security;
+using Raven.Client;
+
+namespace Lumen.AspNetMvc.Raven
+{
+    public class RavenApplicationServiceContext
+    {
+        public RavenApplicationServiceContext(dynamic payload, IUser user, IDocumentSession session)
+        {
+            Payload = Ensure.NotNull(payload, "payload");
+            User = Ensure.NotNull(user, "user");
+            Session = Ensure.NotNull(session, "session");
+        }
+
+        public dynamic Payload { get; private set; }
+        public IDocumentSession Session { get; private set; }
+        public IUser User { get; private set; }
+    }
+}
