2470e71babcf52c09b3d37a648d0c5d552d43f0f
Mattias Rydengren <mattias.rydengren@coderesque.com>
2014-05-30 13:49:57 +0200

    Add sanity 'tests' for custom application service base/pipeline context.

---
+++ src/Lumen.Tests/CustomPipelineContextTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Web.Mvc;
+using Lumen.AspNetMvc;
+using Ninject;
+
+namespace Lumen.Tests
+{
+    public class CustomPipelineContextTests
+    {
+        public interface ICustomApplicationService : IApplicationService
+        {
+            bool IsAuthorized();
+        }
+
+        public abstract class CustomApplicationService : ApplicationService, ICustomApplicationService
+        {
+            public abstract bool IsAuthorized();
+        }
+
+        public abstract class CustomApplicationService<TResult> : ApplicationService<TResult>, ICustomApplicationService
+        {
+            public abstract bool IsAuthorized();
+        }
+
+
+        public class CustomPipelineContext : PipelineContext<ICustomApplicationService>
+        {
+            public CustomPipelineContext(ICustomApplicationService service) : base(service) { }
+        }
+
+        public class CustomApplicationServiceFilter : ApplicationServiceFilter<ApplicationServiceContext, CustomPipelineContext>
+        {
+            public override void Process(ApplicationServiceContext context, CustomPipelineContext pipelineContext)
+            {
+                if (pipelineContext.Service.IsAuthorized())
+                {
+                    return;
+                }
+
+                throw new ApplicationServiceAuthorizationException();
+            }
+        }
+
+        public class MyService : CustomApplicationService
+        {
+            public class Payload { }
+
+            public override bool IsAuthorized()
+            {
+                return true;
+            }
+
+            protected override void ExecuteCore() { }
+        }
+
+
+        public class MyController : ApplicationController
+        {
+            protected override TResult InvokeService<TService, TPayload, TResult>(TPayload payload)
+            {
+                var invoker = DependencyResolver.Current.GetService<ApplicationServiceInvoker<ApplicationServiceContext, CustomPipelineContext>>();
+                return invoker.Invoke<TService, TResult>(new ApplicationServiceContext(payload));
+            }
+
+            public ActionResult My()
+            {
+                return Invoke<MyService, MyService.Payload>();
+            }
+        }
+
+        protected IKernel GetKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<ApplicationServiceInvoker<ApplicationServiceContext, CustomPipelineContext>>().ToSelf().InSingletonScope();
+            kernel.Bind<ApplicationServiceFactory<ApplicationServiceContext>>().ToSelf().InSingletonScope();
+            kernel.Bind<ApplicationServiceFilterProvider<ApplicationServiceContext, CustomPipelineContext>>().ToSelf().InSingletonScope();
+            kernel.Bind<ApplicationServiceFilter<ApplicationServiceContext, CustomPipelineContext>>().To<CustomApplicationServiceFilter>();
+
+            return kernel;
+        }
+    }
+}
